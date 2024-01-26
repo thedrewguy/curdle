2e2b10bc3bf1bd9cef1f51cf749cd1717c0a28d3
Drew Marshall <thedrewguy@gmail.com>
2024-01-25 16:16:41 -0800

    constraints, narrowing

--- src/logic/logic.ts
+++ src/logic/logic.ts
@@ -1,6 +1,7 @@
-import { Letter } from "../data/letters";
+import { Letter, alphabet } from "../data/letters";
 import { guessWords } from "../data/guess-words";
 import { Guess, Guessed } from "../data/types";
+import _ from "lodash";
 
 export function makesValidWord(letters: Letter[]) {
   return guessWords.includes(letters.join(""));
@@ -11,12 +12,50 @@ export function fitsGuessed(guess: Guess, guessed: Guessed) {
 }
 
 const guessed: Guessed = [
-  { letter: "S", color: "green" },
-  { letter: "P", color: "grey" },
   { letter: "E", color: "yellow" },
+  { letter: "M", color: "yellow" },
+  { letter: "C", color: "grey" },
+  { letter: "E", color: "green" },
   { letter: "E", color: "grey" },
-  { letter: "D", color: "grey" },
 ];
 
-console.log(fitsGuessed(["S", "A", "U", "C", "E"], guessed));
-console.log(fitsGuessed(["S", "E", "E", "K", "S"], guessed));
+type Constraint = { min: number; max: number };
+type Constraints = Record<Letter, Constraint>;
+
+function initialConstraints() {
+  return Object.fromEntries(
+    alphabet.map((letter) => [letter, { min: 0, max: 5 }])
+  ) as Constraints;
+}
+
+function narrowConstraints(constraints: Constraints, guessed: Guessed) {
+  const lettersInGuessed = _.uniq(guessed.map((gl) => gl.letter));
+
+  lettersInGuessed.forEach((letter) => {
+    const matchingGuessedLetters = guessed.filter((gl) => gl.letter === letter);
+    const greenish = matchingGuessedLetters.filter(
+      (gl) => gl.color === "green" || gl.color === "yellow"
+    ).length;
+    const grey = matchingGuessedLetters.filter(
+      (gl) => gl.color === "grey"
+    ).length;
+
+    const constraint = constraints[letter];
+
+    if (greenish > constraint.min) {
+      constraint.min = greenish;
+    }
+    if (grey) {
+      constraint.max = greenish;
+    }
+  });
+}
+
+const constraints = initialConstraints();
+
+narrowConstraints(constraints, guessed);
+
+console.dir(constraints, { depth: Infinity });
+
+console.log(fitsGuessed(["M", "E", "L", "E", "E"], guessed));
+console.log(fitsGuessed(["M", "E", "T", "E", "R"], guessed));
