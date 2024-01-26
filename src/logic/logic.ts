2e2408afdfffe57b478ba8cbcdc299b2abed75f1
Drew Marshall <thedrewguy@gmail.com>
2024-01-25 17:21:49 -0800

    rework placement constraint. change narrowConstraints to mutator for now

--- src/logic/logic.ts
+++ src/logic/logic.ts
@@ -11,6 +11,18 @@ export function fitsGuessed(guess: Guess, guessed: Guessed) {
   return false;
 }
 
+function fitsConstraints(guess: Guess, constraints: CountConstraints) {
+  const letterCounts = _.countBy(guess);
+  guess.forEach((letter, index) => {
+    const constraint = constraints[letter];
+    const num = letterCounts[letter];
+    if (num > constraint.max || num < constraint.min) {
+      return false;
+    }
+  });
+  return true;
+}
+
 const guessed: Guessed = [
   { letter: "E", color: "yellow" },
   { letter: "M", color: "yellow" },
@@ -19,30 +31,37 @@ const guessed: Guessed = [
   { letter: "E", color: "grey" },
 ];
 
-type Constraint = {
-  min: number;
-  max: number;
-  placement: (boolean | undefined)[];
-};
-type Constraints = Record<Letter, Constraint>;
+type PlacementConstraint = { yes: Set<Letter>; no: Set<Letter> };
+type PlacementConstraints = [
+  PlacementConstraint,
+  PlacementConstraint,
+  PlacementConstraint,
+  PlacementConstraint,
+  PlacementConstraint
+];
+type CountConstraints = Record<Letter, { min: number; max: number }>;
+type Constraints = { count: CountConstraints; placement: PlacementConstraints };
 
-function initialConstraint(): Constraint {
-  return { min: 0, max: 5, placement: Array(5).fill(undefined) };
-}
+function initialConstraints(): Constraints {
+  const count = Object.fromEntries(
+    alphabet.map((letter) => [letter, { min: 0, max: 5 }])
+  ) as CountConstraints;
 
-function initialConstraints() {
-  return Object.fromEntries(
-    alphabet.map((letter) => [letter, initialConstraint()])
-  ) as Constraints;
+  const placement = Array(5)
+    .fill(undefined)
+    .map((udf) => ({
+      yes: new Set<Letter>(),
+      no: new Set<Letter>(),
+    })) as PlacementConstraints;
+
+  return { count: count, placement: placement };
 }
 
 function narrowConstraints(constraints: Constraints, guessed: Guessed) {
-  const newConstraints = { ...constraints };
+  const { count, placement } = constraints;
+
   _.uniq(guessed.map((gl) => gl.letter)).forEach((letter) => {
-    const newConstraint = {
-      ...constraints[letter],
-      placement: [...constraints[letter].placement],
-    };
+    const letterCountConstraint = count[letter];
     let greenish = 0;
     let grey = 0;
     guessed.forEach((gl, index) => {
@@ -51,35 +70,31 @@ function narrowConstraints(constraints: Constraints, guessed: Guessed) {
       }
       if (gl.color === "green") {
         greenish++;
-        newConstraint.placement[index] = true;
+        placement[index].yes.add(letter);
         return;
       }
       if (gl.color === "yellow") {
         greenish++;
-        newConstraint.placement[index] = false;
+        placement[index].no.add(letter);
         return;
       }
       grey++;
-      newConstraint.placement[index] = false;
+      placement[index].no.add(letter);
     });
 
-    if (greenish > newConstraint.min) {
-      newConstraint.min = greenish;
+    if (greenish > letterCountConstraint.min) {
+      letterCountConstraint.min = greenish;
     }
     if (grey) {
-      newConstraint.max = greenish;
+      letterCountConstraint.max = greenish;
     }
-    newConstraints[letter] = newConstraint;
+    count[letter] = letterCountConstraint;
   });
-  return newConstraints;
 }
 
-const constraints = initialConstraints();
-
-const newConstraints = narrowConstraints(constraints, guessed);
-
-console.dir(constraints, { depth: Infinity });
-console.dir(newConstraints, { depth: Infinity });
+const c = initialConstraints();
+narrowConstraints(c, guessed);
+console.dir(c, { depth: Infinity });
 
 console.log(fitsGuessed(["M", "E", "L", "E", "E"], guessed));
 console.log(fitsGuessed(["M", "E", "T", "E", "R"], guessed));
