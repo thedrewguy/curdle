f9ecfcaa0284bb395f57e3b717200bf5c805d607
Drew Marshall <thedrewguy@gmail.com>
2024-01-27 21:17:01 -0800

    cool header

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -3,18 +3,42 @@ import { LetterCard } from "../grid/LetterCard";
 
 export function Header(props: BoxProps) {
   return (
-    <Box display="flex" alignItems="center" flexDirection="column" {...props}>
-      <Stack spacing={1} direction="row">
-        <LetterCard color="darkred" letter="C" />
-        <LetterCard color="black" letter="U" />
-        <LetterCard color="darkred" letter="R" />
-        <LetterCard color="black" letter="D" />
-        <LetterCard color="darkred" letter="L" />
-        <LetterCard color="black" letter="E" />
-      </Stack>
-      <Typography variant="caption" color="darkred" fontWeight="bold">
-        Cursed Wordle
-      </Typography>
+    <Box display="flex" justifyContent="center">
+      <Box display="flex" flexDirection="column">
+        <Box display="flex">
+          <Box width="50%" />
+          <Box width="8px" />
+          <Typography
+            width="50%"
+            fontWeight="bold"
+            textAlign="center"
+            fontSize=".75rem"
+          >
+            wor
+          </Typography>
+        </Box>
+        <Stack spacing={1} direction="row">
+          <LetterCard color="darkred" letter="C" />
+          <LetterCard color="black" letter="U" />
+          <LetterCard color="darkred" letter="R" />
+          <LetterCard color="black" letter="D" />
+          <LetterCard color="darkred" letter="L" />
+          <LetterCard color="black" letter="E" />
+        </Stack>
+        <Box display="flex">
+          <Typography
+            width="50%"
+            color="darkred"
+            fontWeight="bold"
+            textAlign="center"
+            fontSize=".75rem"
+          >
+            sed
+          </Typography>
+          <Box width="8px" />
+          <Box width="50%" />
+        </Box>
+      </Box>
     </Box>
   );
 }
